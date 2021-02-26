88bce6a6791502747076f9f63fb4767741158dba
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-02-26 15:33:56 -0600

    Update main.java
    Finished byte, short, and long exercises

--- hello_world/src/acadamy/learnProgramming/main.java
+++ hello_world/src/acadamy/learnProgramming/main.java
@@ -11,6 +11,26 @@ public class main {
         System.out.println("Busted max value " + (myMaxIntValue + 1));
         System.out.println("Busted min value " + (myMinIntValue - 1));
 
-        int myMaxTest = 2147483647;
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte min value = " + myMinByteValue);
+        System.out.println("byte max value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short min value = " + myMinShortValue);
+        System.out.println("Short max value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long min value = " + myMinLongValue);
+        System.out.println("Long max value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2147483647234L;
+        System.out.println(bigLongLiteralValue);
     }
 }
