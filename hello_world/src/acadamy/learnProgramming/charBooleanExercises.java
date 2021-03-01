a2c758d958cae87dcfdd52a8a1c31cfabacf90f4
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-02-28 18:45:48 -0600

    Create charBooleanExercises.java
    completed string and boolean exercises

---
+++ hello_world/src/acadamy/learnProgramming/charBooleanExercises.java
@@ -0,0 +1,19 @@
+package acadamy.learnProgramming;
+
+public class charBooleanExercises {
+    public static void main(String[] args) {
+        
+        char myChar = 'J';
+        char myUniCodeChar = '\u0044';
+        char myCopyRightChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUniCodeChar);
+        System.out.println(myCopyRightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+    }
+    
+}
