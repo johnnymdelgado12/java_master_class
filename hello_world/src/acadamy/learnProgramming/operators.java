18a33df6c18a262ad7c95e42e32484dba8055afa
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-02-28 19:30:33 -0600

    Update operators.java
    comleted second part of operators exercises

--- hello_world/src/acadamy/learnProgramming/operators.java
+++ hello_world/src/acadamy/learnProgramming/operators.java
@@ -18,5 +18,23 @@ public class operators {
 
         result = result % 3;
         System.out.println("4 % 3 = " + result);
+
+        result++;
+        System.out.println("1 + 1 = " + result);
+
+        result--;
+        System.out.println("2 - 1 = " + result);
+
+        result +=2;
+        System.out.println("1 + 2 = " + result);
+
+        result *=10;
+        System.out.println("3 * 10 = " + result);
+
+        result /= 3;
+        System.out.println("30 / 3 = " + result);
+
+        result -=5;
+        System.out.println("10 - 5 = " + result);
     }
 }
