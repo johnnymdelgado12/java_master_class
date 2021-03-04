4d386a9ee2c549c5ffdf963324f79eb3ff8a34ca
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-03-03 19:29:29 -0600

    Update operators.java
    created test expression

--- hello_world/src/acadamy/learnProgramming/operators.java
+++ hello_world/src/acadamy/learnProgramming/operators.java
@@ -89,6 +89,12 @@ public class operators {
         if(!isZero){
             System.out.println("Got Some Remainder");
         }
+
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50){
+            System.out.println("This is an expression!");
+        }
         
     }
 }
